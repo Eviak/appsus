575dc380e3074626fff34f7301ae2e19d4b32bc1
Eviak <karbiane2@gmail.com>
2022-04-27 20:29:05 +0300

    added to mail app button for dev

--- pages/home.jsx
+++ pages/home.jsx
@@ -2,6 +2,8 @@ const { Link } = ReactRouterDOM
 
 export function Home() {
   return <section className="home">
+
+      <Link to="mail?status=inbox" >To Mail app</Link>
       <Link to="notes">To Notes app</Link> 
       </section>
 }
