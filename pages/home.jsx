25bef64ccba164eeb3c95918f32afddd2afc159b
Eviak <karbiane2@gmail.com>
2022-04-30 22:40:12 +0300

    worked on home css

--- pages/home.jsx
+++ pages/home.jsx
@@ -2,6 +2,7 @@ const { Link } = ReactRouterDOM
 
 export function Home() {
   return <section className="home">
+      <img src="../assets/img/home-bg.png" alt="Background image" />
 
       <Link to="mail?status=inbox" >To Mail app</Link>
       <Link to="notes">To Notes app</Link> 
