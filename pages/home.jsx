8774bc8ff4540122bd0a1f697d1e0e598707ca70
Eviak <karbiane2@gmail.com>
2022-04-30 22:58:48 +0300

    worked on homepage buttons and background

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,10 +1,14 @@
 const { Link } = ReactRouterDOM
 
 export function Home() {
-  return <section className="home">
+  return (
+    <section className="home flex-col align-center">
       <img src="../assets/img/home-bg.png" alt="Background image" />
 
-      <Link to="mail?status=inbox" >To Mail app</Link>
-      <Link to="notes">To Notes app</Link> 
-      </section>
+      <div className="to-apps-btns">
+        <Link to="mail?status=inbox">To Mail app</Link>
+        <Link to="notes">To Notes app</Link>
+      </div>
+    </section>
+  )
 }
